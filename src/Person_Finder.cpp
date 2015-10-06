3b1b3150e1148e027be895b974b4d0ad7b02b9df
John Brewer <jbrewer@jera.com>
2015-10-05 19:33:29 -0700

    Fix indentation.

--- src/Person_Finder.cpp
+++ src/Person_Finder.cpp
@@ -24,77 +24,77 @@ using namespace cv;
 bool _inited;
 
 Mat findPerson(Mat mat1) {
-  Mat equalized;
+    Mat equalized;
     
-  double min, max;
+    double min, max;
     
-  MatIterator_<uint16_t> it, end;
-  for( it = mat1.begin<uint16_t>(), end = mat1.end<uint16_t>(); it != end; ++it) {
-    if (*it < min_temp) {
-        *it = min_temp;
-    } else if (*it > max_temp) {
-        *it = min_temp;
+    MatIterator_<uint16_t> it, end;
+    for( it = mat1.begin<uint16_t>(), end = mat1.end<uint16_t>(); it != end; ++it) {
+        if (*it < min_temp) {
+            *it = min_temp;
+        } else if (*it > max_temp) {
+            *it = min_temp;
+        }
     }
-  }
     
-  // minMaxLoc(mat1, &min, &max);
-  // fprintf(stderr, "min: %f max: %f\n", min, max);
-  normalize(mat1, equalized, 0, 16384, cv::NORM_MINMAX);
-  equalized.convertTo(equalized, CV_8UC1, 0.015625);
+    // minMaxLoc(mat1, &min, &max);
+    // fprintf(stderr, "min: %f max: %f\n", min, max);
+    normalize(mat1, equalized, 0, 16384, cv::NORM_MINMAX);
+    equalized.convertTo(equalized, CV_8UC1, 0.015625);
 
-  // fprintf(stderr, "equalized.rows = %d, equalized.cols = %d\n", equalized.rows, equalized.cols);
-  Mat rgbMat(equalized.rows, equalized.cols, CV_8UC3);
-  cvtColor(equalized, rgbMat, CV_GRAY2RGB);
+    // fprintf(stderr, "equalized.rows = %d, equalized.cols = %d\n", equalized.rows, equalized.cols);
+    Mat rgbMat(equalized.rows, equalized.cols, CV_8UC3);
+    cvtColor(equalized, rgbMat, CV_GRAY2RGB);
     
 #if 1
-  Mat mask;
+    Mat mask;
     
-  threshold(equalized, mask, 200, 255, THRESH_BINARY);
+    threshold(equalized, mask, 200, 255, THRESH_BINARY);
     
-  vector<vector<cv::Point> > contours;
-  findContours(mask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-  vector<cv::Point> biggestContour;
-  int biggestArea = 0;
-  for (const auto& contour : contours) {
-    int currentArea = contourArea(contour);
-    if (currentArea > biggestArea) {
-      biggestArea = currentArea;
-      biggestContour = contour;
+    vector<vector<cv::Point> > contours;
+    findContours(mask, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+    vector<cv::Point> biggestContour;
+    int biggestArea = 0;
+    for (const auto& contour : contours) {
+        int currentArea = contourArea(contour);
+        if (currentArea > biggestArea) {
+            biggestArea = currentArea;
+            biggestContour = contour;
+        }
     }
-  }
-  // fprintf(stderr, "Biggest area %d\n", biggestArea);
-  if (biggestArea > 0) {
-    Moments mu = moments(biggestContour);
-    int x = mu.m10/mu.m00;
-    int y = mu.m01/mu.m00;
-    Scalar green(0, 255, 0);
-    drawContours(rgbMat, vector<vector<cv::Point> >{biggestContour}, -1, green);
-    Scalar blue(0, 0, 255);
-    int right = rgbMat.cols;
-    int bottom = rgbMat.rows;
-    cv::line(rgbMat, cv::Point(0,y), cv::Point(right,y), cv::Scalar(0, 0 , 255));
-    cv::line(rgbMat, cv::Point(x,0), cv::Point(x,bottom), cv::Scalar(0, 0 , 255));
-    int matX = rgbMat.cols / 2;
-    int matY = rgbMat.rows / 2;
+    // fprintf(stderr, "Biggest area %d\n", biggestArea);
+    if (biggestArea > 0) {
+        Moments mu = moments(biggestContour);
+        int x = mu.m10/mu.m00;
+        int y = mu.m01/mu.m00;
+        Scalar green(0, 255, 0);
+        drawContours(rgbMat, vector<vector<cv::Point> >{biggestContour}, -1, green);
+        Scalar blue(0, 0, 255);
+        int right = rgbMat.cols;
+        int bottom = rgbMat.rows;
+        cv::line(rgbMat, cv::Point(0,y), cv::Point(right,y), cv::Scalar(0, 0 , 255));
+        cv::line(rgbMat, cv::Point(x,0), cv::Point(x,bottom), cv::Scalar(0, 0 , 255));
+        int matX = rgbMat.cols / 2;
+        int matY = rgbMat.rows / 2;
         
-    float deltaX = (float)(x - matX) / rgbMat.cols;
-    float deltaY = -(float)(y - matY) / rgbMat.rows;
+        float deltaX = (float)(x - matX) / rgbMat.cols;
+        float deltaY = -(float)(y - matY) / rgbMat.rows;
         
-    if (fabs(deltaX) < minDeltaX) {
-      deltaX = 0;
+        if (fabs(deltaX) < minDeltaX) {
+            deltaX = 0;
+        }
+        if (fabs(deltaY) < minDeltaY) {
+            deltaY = 0;
+        }
+        
+        // fprintf(stderr, "%f, %f\n", deltaX, deltaY);
+        // [_galileo panBy:deltaY * 180];
+        // [_galileo tiltBy:deltaX * 180];
+    } else {
+        // [_galileo panBy:0];
+        // [_galileo tiltBy:0];
     }
-    if (fabs(deltaY) < minDeltaY) {
-      deltaY = 0;
-    }
-        
-    // fprintf(stderr, "%f, %f\n", deltaX, deltaY);
-    // [_galileo panBy:deltaY * 180];
-    // [_galileo tiltBy:deltaX * 180];
-  } else {
-    // [_galileo panBy:0];
-    // [_galileo tiltBy:0];
-  }
 #endif
 
-  return rgbMat;
+    return rgbMat;
 }
