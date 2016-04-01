62dadecad922af7a812ed6923fb2e53c291d900a
John Brewer <jbrewer@jera.com>
2016-03-31 18:04:40 -0700

    Now drives right wheel forward.

--- src/Motor_Driver.py
+++ src/Motor_Driver.py
@@ -5,6 +5,7 @@
 
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
+from time import sleep
  
 GPIO.setup("P8_12", GPIO.OUT)
 GPIO.output("P8_12", GPIO.LOW)
@@ -17,7 +18,16 @@ GPIO.setup("P8_16", GPIO.OUT)
 GPIO.output("P8_16", GPIO.LOW)
 
 PWM.start("P9_14", 0)
-PWM.start("P9_33", 0)
+PWM.start("P9_16", 0)
 
 GPIO.cleanup()
 
+GPIO.output("P8_12", GPIO.LOW)
+GPIO.output("P8_10", GPIO.HIGH)
+PWM.set_duty_cycle("P9_14", 50)
+
+sleep(1)
+
+PWM.set_duty_cycle("P9_14", 0)
+GPIO.output("P8_12", GPIO.LOW)
+GPIO.output("P8_10", GPIO.LOW)
