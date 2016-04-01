617b750b0ef108195b478184b73442a3826280bf
John Brewer <jbrewer@jera.com>
2016-03-31 19:08:09 -0700

    Factor out functions.

--- src/Motor_Driver.py
+++ src/Motor_Driver.py
@@ -7,27 +7,53 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 from time import sleep
  
-GPIO.setup("P8_12", GPIO.OUT)
-GPIO.output("P8_12", GPIO.LOW)
-GPIO.setup("P8_10", GPIO.OUT)
-GPIO.output("P8_10", GPIO.LOW)
+def init_pins():
+    GPIO.setup("P8_12", GPIO.OUT)
+    GPIO.output("P8_12", GPIO.LOW)
+    GPIO.setup("P8_10", GPIO.OUT)
+    GPIO.output("P8_10", GPIO.LOW)
 
-GPIO.setup("P8_14", GPIO.OUT)
-GPIO.output("P8_14", GPIO.LOW)
-GPIO.setup("P8_16", GPIO.OUT)
-GPIO.output("P8_16", GPIO.LOW)
+    GPIO.setup("P8_14", GPIO.OUT)
+    GPIO.output("P8_14", GPIO.LOW)
+    GPIO.setup("P8_16", GPIO.OUT)
+    GPIO.output("P8_16", GPIO.LOW)
 
-PWM.start("P9_14", 0)
-PWM.start("P9_16", 0)
+    PWM.start("P9_14", 0)
+    PWM.start("P9_16", 0)
 
-GPIO.cleanup()
+def move(leftPercent, rightPercent, duration):
+    if (leftPercent > 0):
+        GPIO.output("P8_14", GPIO.LOW)
+        GPIO.output("P8_16", GPIO.HIGH)
+    else:
+        GPIO.output("P8_14", GPIO.HIGH)
+        GPIO.output("P8_16", GPIO.LOW)
 
-GPIO.output("P8_12", GPIO.LOW)
-GPIO.output("P8_10", GPIO.HIGH)
-PWM.set_duty_cycle("P9_14", 50)
+    if (rightPercent > 0):
+        GPIO.output("P8_12", GPIO.LOW)
+        GPIO.output("P8_10", GPIO.HIGH)
+    else:
+        GPIO.output("P8_12", GPIO.HIGH)
+        GPIO.output("P8_10", GPIO.LOW)
+        
+    PWM.set_duty_cycle("P9_16", abs(leftPercent))
+    PWM.set_duty_cycle("P9_14", abs(rightPercent))
 
-sleep(1)
+    sleep(duration);
 
-PWM.set_duty_cycle("P9_14", 0)
-GPIO.output("P8_12", GPIO.LOW)
-GPIO.output("P8_10", GPIO.LOW)
+    GPIO.output("P8_12", GPIO.LOW)
+    GPIO.output("P8_10", GPIO.LOW)
+
+    GPIO.output("P8_14", GPIO.LOW)
+    GPIO.output("P8_16", GPIO.LOW)
+
+    PWM.set_duty_cycle("P9_16", 0)
+    PWM.set_duty_cycle("P9_14", 0)
+
+try:
+    init_pins()
+    move(-100, -100, 1)
+#    move(-70, 70, 0.05)
+finally:
+    GPIO.cleanup()
+
